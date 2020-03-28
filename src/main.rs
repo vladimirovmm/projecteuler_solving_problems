a123c067427d2d2f46f9a163553be9064f667ff7
VMM <vladimirov.m.m@alex-its.uz>
2020-03-29 02:35:33 +0500

    Задача 2
    Signed-off-by: VMM <vladimirov.m.m@alex-its.uz>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 
 // Задача 1
 // mod lesson1;
+// Задача 2
+mod lesson2;
 
 fn main() {
     // Задача 1
     // lesson1::run_variant_1();
     // lesson1::run_variant_2();
-
+    lesson2::run();
 }
