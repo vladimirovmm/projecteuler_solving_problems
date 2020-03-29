5a7279cb18d305ed9e0f3a1180be013acde8f214
VMM <vladimirov.m.m@alex-its.uz>
2020-03-29 21:48:50 +0500

    Ссылки на задачу
    Signed-off-by: VMM <vladimirov.m.m@alex-its.uz>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-
 // Задача 1
 // mod lesson1;
 // Задача 2
 // mod lesson2;
 // Задача 3
-mod lesson3;
+// mod lesson3;
+// Задача 4
+mod lesson4;
 
 fn main() {
     // Задача 1
@@ -18,4 +19,6 @@ fn main() {
     // lesson3::run_variant_2();
     // lesson3::run_variant_1();
 
+    // Задача 4
+    lesson4::run();
 }
